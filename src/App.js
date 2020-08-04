6a0d8e5f4e19747641be61ce08a0671d76ad0bd9
Your Name <you@example.com>
2020-08-04 14:45:53 +0800

    bzhang

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import {Button,message} from 'antd'
 
 class App extends Component {
+    handleClick=()=>{
+        message.success("成功了。。。")
+    }
     render() {
         return (
             <div>
-                App
+                <Button type="primary" onClick={this.handleClick}>学习</Button>
             </div>
         );
     }
